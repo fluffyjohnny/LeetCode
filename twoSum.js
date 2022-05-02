364014f6a6f4af58e47672ad36542ce8cab5ad67
John Chia <john.chia0106@gmail.com>
2022-05-01 20:18:46 -0700

    add #20

--- twoSum.js
+++ twoSum.js
@@ -7,17 +7,17 @@
 const twoSum = function(nums, target) {
   let result = [];
 
-  const index_map = new Map();
+  const indexMap = new Map();
 
 
   for (let i = 0; i < nums.length; i++) {
     let difference = target - nums[i];
-    if (index_map.has(difference)) {
+    if (indexMap.has(difference)) {
       result[1] = i;
-      result[0] = index_map.get(difference);
+      result[0] = indexMap.get(difference);
       break;
     } else {
-      index_map.set(nums[i], i);
+      indexMap.set(nums[i], i);
     }
   }
   return result;
