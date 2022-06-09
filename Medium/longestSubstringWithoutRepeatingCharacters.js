75de4ec8a170f508c12d02e74f92990d7d74c926
John Chia <john.chia0106@gmail.com>
2022-06-09 15:40:51 -0700

    add comments

--- Medium/longestSubstringWithoutRepeatingCharacters.js
+++ Medium/longestSubstringWithoutRepeatingCharacters.js
@@ -3,18 +3,23 @@ const lengthOfLongestSubstring = function (s) {
   let longestStringLength = 0;
 
   for (let i = 0; i < s.length; i++) {
+    // Attempt to get the current character's position in the current string
     const currentCharPosition = currentString.indexOf(s[i]);
 
+    // Check if the current character exists in the current string
     if (currentCharPosition !== -1) {
+      // Chop the array off after the occurence of the character
       currentString.splice(0, currentCharPosition + 1);
     }
 
+    // Add the current character to the array
     currentString.push(s[i]);
 
+    // Store the current string length if bigger than the existing record
     longestStringLength = Math.max(longestStringLength, currentString.length);
   }
 
   return longestStringLength;
 };
 
-console.log(lengthOfLongestSubstring("abbbabab"));
+console.log(lengthOfLongestSubstring("abbfqefqefqbabab"));
