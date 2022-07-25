f40dfb73a37ba77781665f318d750e2dba8a96d8
John Chia <john.chia0106@gmail.com>
2022-07-25 13:52:57 -0700

    submit searchInsert.js

--- Easy/searchInsert.js
+++ Easy/searchInsert.js
@@ -4,5 +4,15 @@
  * @return {number}
  */
 const searchInsert = function (nums, target) {
-
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i;
+    }
+    if (nums[i] > target) {
+      return i;
+    }
+    if (target > nums[nums.length - 1]) {
+      return nums.length;
+    }
+  }
 };
