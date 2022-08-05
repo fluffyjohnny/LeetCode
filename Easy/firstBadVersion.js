75808c5872b9a94da9e5df08157e482344db1185
John Chia <john.chia0106@gmail.com>
2022-08-04 23:54:27 -0700

    comeplete firstbadversion.js

--- Easy/firstBadVersion.js
+++ Easy/firstBadVersion.js
@@ -12,12 +12,24 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
- var solution = function(isBadVersion) {
+const solution = function (isBadVersion) {
   /**
    * @param {integer} n Total versions
    * @return {integer} The first bad version
    */
-  return function(n) {
-      
+  return function (n) {
+    let start = 1;
+    let end = n;
+
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+
+      if (isBadVersion(mid)) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
   };
 };
