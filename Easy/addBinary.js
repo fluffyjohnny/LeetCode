283d495945b36b69e522032faefbbbeeafb890f6
John Chia <john.chia0106@gmail.com>
2022-08-16 22:17:37 -0700

    add easy question 67

--- Easy/addBinary.js
+++ Easy/addBinary.js
@@ -3,6 +3,27 @@
  * @param {string} b
  * @return {string}
  */
-const addBinary = function(a, b) {
-  
+const addBinary = function (a, b) {
+  let result = "";
+
+  let firstLength = a.length - 1;
+  let secondLength = b.length - 1;
+
+  let carry = 0;
+
+  const reverse = (str) => {
+    return str.split('').reverse().join('');
+  };
+
+  while (firstLength >= 0 || secondLength >= 0) {
+    let totalSum = carry;
+
+    if (firstLength >= 0) totalSum += a[firstLength--] - "0";
+    if (secondLength >= 0) totalSum += b[secondLength--] - "0";
+
+    carry = totalSum > 1 ? 1 : 0;
+    result += totalSum % 2;
+  }
+  if (carry !== 0) result += carry;
+  return reverse(result);
 };
