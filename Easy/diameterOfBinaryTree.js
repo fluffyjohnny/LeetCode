164dda21f8270d8d2c478c52157543b118e8362b
John Chia <john.chia0106@gmail.com>
2022-08-22 14:33:04 -0700

    implement dfs function for finding diameter of binary tree

--- Easy/diameterOfBinaryTree.js
+++ Easy/diameterOfBinaryTree.js
@@ -10,6 +10,22 @@
  * @param {TreeNode} root
  * @return {number}
  */
-const diameterOfBinaryTree = function(root) {
-    
+const diameterOfBinaryTree = function (root) {
+  if (!root) return 0;
+
+  let max = 0;
+
+  const dfs = (node) => {
+    if (!node) return 0;
+
+    let left = dfs(node.left);
+    let right = dfs(node.right);
+
+    max = Math.max(left + right, max);
+
+    return Math.max(left, right) + 1;
+  };
+
+  dfs(root);
+  return max;
 };
