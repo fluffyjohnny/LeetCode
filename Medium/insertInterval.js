9b3a0c1b6eb3cd8778f2a2702ef3f9bfc58dd626
John Chia <john.chia0106@gmail.com>
2022-08-23 23:33:06 -0700

    finish implementing logic for insert interval

--- Medium/insertInterval.js
+++ Medium/insertInterval.js
@@ -3,6 +3,36 @@
  * @param {number[]} newInterval
  * @return {number[][]}
  */
-const insert = function(intervals, newInterval) {
-    
+const insert = function (intervals, newInterval) {
+
+  const result = [];
+
+  if (!intervals.length) {
+    return [newInterval];
+  }
+
+  const index = intervals.findIndex(([a]) => newInterval[0] <= a);
+
+  if (index === -1) {
+    intervals.push(newInterval);
+  } else {
+    intervals.splice(index, 0, newInterval);
+  }
+
+  result.push(intervals[0]);
+
+  let index2 = 1;
+  while (index2 <= intervals.length - 1) {
+    const [a, b] = result[result.length - 1];
+    const [c, d] = intervals[index2];
+
+    if (a <= c && c <= b) {
+      result[result.length - 1] = [a, Math.max(b, d)];
+    } else {
+      result.push(intervals[index2]);
+    }
+    index2 += 1;
+  }
+
+  return result;
 };
