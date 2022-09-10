d2cdd6ff24179f94032993e8485924ecc16b8bf8
John Chia <john.chia0106@gmail.com>
2022-09-09 23:46:30 -0700

    finish implementing function to sort array to bst

--- Easy/convertArrayToBinaryTree.js
+++ Easy/convertArrayToBinaryTree.js
@@ -11,5 +11,18 @@
  * @return {TreeNode}
  */
 const sortedArrayToBST = function(nums) {
-  
+  const convToBST = (arr, start, end) => {
+    if (start > end) {
+      return null;
+    }
+    const mid = Math.ceil((start + end) / 2);
+    const root = new TreeNode(nums[mid]);
+
+    root.left = convToBST(arr, start, mid - 1);
+    root.right = convToBST(arr, mid + 1, end);
+
+    return root;
+  };
+
+  return convToBST(nums, 0, nums.length - 1);
 };
