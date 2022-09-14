26fbd96650c0eb8574b998fc34dd636ecc0e957d
John Chia <john.chia0106@gmail.com>
2022-09-13 23:53:14 -0700

    finish implementing function and recursions

--- Medium/BinaryTreeLevelOrderTraversal.js
+++ Medium/BinaryTreeLevelOrderTraversal.js
@@ -10,6 +10,27 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-const levelOrder = function(root) {
-  
+const levelOrder = function (root) {
+  let count = 0;
+  let depths = {};
+
+  // Function to separate levels by storing in a has
+  const countDepth = (node, count) => {
+    if (!node) {
+      return;
+    }
+
+    if (count in depths) {
+      depths[count].push(node.val);
+    } else {
+      depths[count] = [];
+      depths[count].push(node.val);
+    }
+
+    countDepth(node.left, count + 1);
+    countDepth(node.right, count + 1);
+  };
+
+  countDepth(root, count);
+  return Object.values(depths);
 };
