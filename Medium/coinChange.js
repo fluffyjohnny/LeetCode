2a87fcebf2a253f24fe44806a4a41d741a74fff3
John Chia <john.chia0106@gmail.com>
2022-09-21 11:52:01 -0700

    implement question

--- Medium/coinChange.js
+++ Medium/coinChange.js
@@ -3,6 +3,14 @@
  * @param {number} amount
  * @return {number}
  */
-const coinChange = function(coins, amount) {
-    
+const coinChange = function (coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let coin of coins) {
+    for (let i = coin; i < dp.length; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+    }
+  }
+  return dp[dp.length - 1] === Infinity ? -1 : dp[dp.length - 1];
 };
