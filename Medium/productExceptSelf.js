0ba6d7fd3d36a3c3615192bf79c67141e6ae2021
John Chia <john.chia0106@gmail.com>
2022-09-21 12:00:22 -0700

    implement function

--- Medium/productExceptSelf.js
+++ Medium/productExceptSelf.js
@@ -2,6 +2,18 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const productExceptSelf = function(nums) {
-    
+const productExceptSelf = function (nums) {
+  const result = [];
+  let product = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = product;
+    product *= nums[i];
+  }
+
+  product = 1;
+  for (let j = nums.length - 1; j >= 0; j--) {
+    result[j] *= product;
+    product *= nums[j];
+  }
+  return result;
 };
