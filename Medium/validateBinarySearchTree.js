da75c5f92b737803706f6b7795cc8f75cf353dff
John Chia <john.chia0106@gmail.com>
2022-09-25 09:23:46 -0700

    finish implementing medium question 98

--- Medium/validateBinarySearchTree.js
+++ Medium/validateBinarySearchTree.js
@@ -10,6 +10,37 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-const isValidBST = function(root) {
-    
+const isValidBST = function (root) {
+  let prev;
+
+  let valid = true;
+
+  const inorder = (node) => {
+    if (!node || !valid) {
+      return;
+    }
+
+    inorder(node.left);
+
+    if (prev >= node.val) {
+      /**
+       * In order traversal, for valid binary tree,
+       * nodes value keep increasing. So, considering that
+       * just keep the previous value in memory,
+       * if the previous value is greater than current node value
+       * it is violation.
+       * Set a valid flag to false, and early return.
+       **/
+      valid = false;
+      return;
+    }
+
+    prev = node.val;
+
+    inorder(node.right);
+  };
+
+  inorder(root);
+
+  return valid;
 };
