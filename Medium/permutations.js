364c89b0a27459e905a7b8e0a40f53f5da906c7d
John Chia <john.chia0106@gmail.com>
2022-10-03 21:31:13 -0700

    finish implementing medium question 46

--- Medium/permutations.js
+++ Medium/permutations.js
@@ -2,6 +2,40 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-const permute = function(nums) {
-    
+const permute = function (letters) {
+  const used = new Set(); // Keep track of what we have used
+  const path = []; // Current potiential answer array
+  const res = []; // Result array to be returned
+
+  const dfs = () => {
+    // If path is same length as nums, we know we have an answer. Push it to res array
+    if (path.length === letters.length) {
+      res.push([...path]); // We use spread operator to clone since arrays are pass by reference
+    }
+
+    // Every DFS we loop all numbers
+    for (let i = 0; i < letters.length; i++) {
+      // We can skip these numbers if they have been used
+      if (used.has(letters[i])) continue;
+
+      // Add to our potienial answer array and make it used by adding to used set
+      path.push(letters[i]);
+      used.add(letters[i]);
+
+      // After adding, we call DFS again. DFS will continue till we hit the base case above
+      // Think of this as just continuing down a path till we have an answer
+      dfs();
+
+      // Once we pop out of DFS, we need to remove from path array and remove from used Set
+      // This will let it be used later in further paths
+      path.pop();
+      used.delete(letters[i]);
+    }
+  };
+
+  // Start DFS
+  // All variables are global, no need to pass in anything
+  dfs();
+
+  return res;
 };
