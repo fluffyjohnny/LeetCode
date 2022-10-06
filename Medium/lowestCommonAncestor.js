b734f63d83757789a1454921e762f9e3eb0ecb7c
John Chia <john.chia0106@gmail.com>
2022-10-05 22:22:15 -0700

    finish medium question 236

--- Medium/lowestCommonAncestor.js
+++ Medium/lowestCommonAncestor.js
@@ -11,6 +11,21 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-const lowestCommonAncestor = function(root, p, q) {
-    
+const lowestCommonAncestor = function (root, p, q) {
+  const dfs = (node) => {
+    if (node === null) {
+      return null;
+    }
+
+    if (node === p || node === q) {
+      return node;
+    }
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+
+    return left && right ? node : left || right;
+  };
+
+  return dfs(root);
 };
