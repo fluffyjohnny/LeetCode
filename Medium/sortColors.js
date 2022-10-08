05a427adffb1016f8cf4b42467225868a33f4c48
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-10-07 23:22:14 -0700

    finish implementing question 75

--- Medium/sortColors.js
+++ Medium/sortColors.js
@@ -2,6 +2,21 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const sortColors = function(nums) {
-  
+const sortColors = function (nums) {
+  let zero = [],
+    one = [],
+    two = [];
+  for (let num of nums) {
+    if (num === 0) {
+      zero.push(0);
+    } else if (num === 1) {
+      one.push(1);
+    } else {
+      two.push(2);
+    }
+  }
+  nums.length = 0;
+  nums.push(...zero);
+  nums.push(...one);
+  nums.push(...two);
 };
