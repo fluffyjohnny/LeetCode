1cc5cb7bc5ca14013c30ca7bb90dac4e6959bfb5
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-10-10 11:12:29 -0700

    add and implement medium question 139

---
+++ Medium/wordBreak.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+const wordBreak = function(s, wordDict) {
+
+  const set = new Set(wordDict);
+
+  const visited = new Set();
+  const q = [0];
+
+  while (q.length) {
+    const start = q.shift();
+
+    if (!visited.has(start)) {
+      for (let end = start + 1; end <= s.length; end++) {
+        if (set.has(s.slice(start, end))) {
+          if (end === s.length) return true;
+          q.push(end);
+        }
+      }
+      console.log(start)
+      visited.add(start);
+    }
+  }
+  return false;
+};
+
+
+console.log(wordBreak('leetcode', ['leet', 'code']));
