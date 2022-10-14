29e88115a6274657a20597e2fc0969c82db8ba2c
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-10-14 12:17:16 -0700

    complete medium question 54

--- Medium/spiralOrder.js
+++ Medium/spiralOrder.js
@@ -2,6 +2,17 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
-const spiralOrder = function(matrix) {
-    
+const spiralOrder = function (matrix) {
+  const res = [];
+  while (matrix.length) {
+    const first = matrix.shift();
+    res.push(...first);
+    for (const m of matrix) {
+      let val = m.pop();
+      if (val) res.push(val);
+      m.reverse();
+    }
+    matrix.reverse();
+  }
+  return res;
 };
