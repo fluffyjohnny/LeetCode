54d5bc40ae8d0236156ff6a2164b11eddf8bc1e2
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-10-17 10:07:53 -0700

    finish implementing medium question 199

--- Medium/rightSideView.js
+++ Medium/rightSideView.js
@@ -10,6 +10,21 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-const rightSideView = function(root) {
-    
+const rightSideView = function (root) {
+  if (!root) return [];
+  let vals = [];
+  let q = [root];
+
+  while (q.length) {
+    let size = q.length;
+    let lvl = [];
+    for (let i = 0; i < size; i++) {
+      let c = q.shift();
+      lvl.push(c.val);
+      if (c.left) q.push(c.left);
+      if (c.right) q.push(c.right);
+    }
+    vals.push(lvl.at(-1));
+  }
+  return vals;
 };
