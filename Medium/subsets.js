9eda58ef9eefafe53a8fffd9cca678c610d0638d
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-10-17 09:40:36 -0700

    finish implementing medium question 78

--- Medium/subsets.js
+++ Medium/subsets.js
@@ -2,6 +2,20 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-const subsets = function(nums) {
-    
+const subsets = function (nums) {
+  let res = [[]],
+    appendarr = [];
+
+  for (let num of nums) {
+    appendarr = [];
+    for (let entry of res) {
+      appendarr.push([...entry, num]);
+    }
+
+    res.push(...appendarr);
+  }
+
+  return res;
 };
+
+console.log(subsets([1, 2, 3]))
