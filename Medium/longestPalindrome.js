baa648fdf0847b0abc15f521a32da918744208a5
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-10-18 17:07:14 -0700

    implement medium question 5

--- Medium/longestPalindrome.js
+++ Medium/longestPalindrome.js
@@ -2,6 +2,31 @@
  * @param {string} s
  * @return {string}
  */
-const longestPalindrome = function(s) {
-    
+const longestPalindrome = function (s) {
+  if (s.length < 1 || s === null) return "";
+
+  let longest = "";
+
+  for (let i = 0; i < s.length; i++) {
+    let oddPalindrome = expandFromCenter(s, i, i);
+    let evenPalindrome = expandFromCenter(s, i - 1, i);
+
+    if (oddPalindrome.length > longest.length) {
+      longest = oddPalindrome;
+    }
+    if (evenPalindrome.length > longest.length) {
+      longest = evenPalindrome;
+    }
+  }
+  return longest;
+};
+
+const expandFromCenter = (s, l, r) => {
+  let i = 0;
+  while (s[l - i] && s[l - i] === s[r + i]) {
+    i++;
+  }
+  i--;
+
+  return s.slice(l - i, r + i + 1);
 };
