30300768e2614a39e6b25b3e311a06c2eb675b99
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-03 10:15:36 -0700

    implement medium question 230

--- Medium/kthSmallest.js
+++ Medium/kthSmallest.js
@@ -11,6 +11,17 @@
  * @param {number} k
  * @return {number}
  */
-const kthSmallest = function(root, k) {
-    
+const kthSmallest = function (root, k) {
+  let vals = [];
+  const inOrder = (node) => {
+    if (vals.length !== k) {
+      //no need to keep going after reach k-th number
+      if (node.left) inOrder(node.left); //go left first
+      vals.push(node.val); //finished going left, now start adding values
+      if (node.right) inOrder(node.right); //if have right, go there and repeat process
+    }
+  };
+
+  inOrder(root); // IFFE Immediately Invoking Function Expression, starting from root.
+  return vals[k - 1]; //return element, but as i mentioned in the descript, don't need this full array, just the k-th elm
 };
