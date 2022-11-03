f302fd0d84360feda2e24bfd2826470439b4397b
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-03 10:58:51 -0700

    implement medium question 7

--- Medium/reverse.js
+++ Medium/reverse.js
@@ -2,6 +2,8 @@
  * @param {number} x
  * @return {number}
  */
-const reverse = function(x) {
-    
+const reverse = function (x) {
+  const absReversed = Math.abs(x).toString().split("").reverse().join("");
+  if (absReversed > 2 ** 31) return 0;
+  return absReversed * Math.sign(x);
 };
