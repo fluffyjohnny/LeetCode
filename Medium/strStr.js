6aec3e4454b3418954907182a609f2fab32dbb0f
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-08 09:23:34 -0800

    implement medium question 28

--- Medium/strStr.js
+++ Medium/strStr.js
@@ -3,6 +3,13 @@
  * @param {string} needle
  * @return {number}
  */
-const strStr = function(haystack, needle) {
-    
+const strStr = function (haystack, needle) {
+  if (!needle.length) return 0;
+  for (let i in haystack)
+    if (
+      haystack[i] === needle[0] &&
+      haystack.substring(i, Number(i) + needle.length) === needle
+    )
+      return i;
+  return -1;
 };
