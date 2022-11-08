751768f669d0864b1d37212f055bd7b03b95963f
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-07 16:11:10 -0800

    implement medium question 2

--- Medium/generateParenthesis.js
+++ Medium/generateParenthesis.js
@@ -2,6 +2,20 @@
  * @param {number} n
  * @return {string[]}
  */
-const generateParenthesis = function(n) {
-    
+const generateParenthesis = function (n) {
+  const result = [];
+
+  const backtracking = (open, close, cur) => {
+    if (close > open || open > n) return;
+    if (open === n && close === n) {
+      result.push(cur);
+      return;
+    }
+    backtracking(open + 1, close, cur + "(");
+    backtracking(open, close + 1, cur + ")");
+  };
+
+  backtracking(0, 0, '');
+
+  return result;
 };
