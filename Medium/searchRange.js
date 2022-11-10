bb01329e434017beab62a9f43e080095711771fa
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-09 20:25:09 -0800

    implement medium question 34

--- Medium/searchRange.js
+++ Medium/searchRange.js
@@ -3,6 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-const searchRange = function(nums, target) {
-    
+const searchRange = function (nums, target) {
+  const idx = [];
+  let empty = true;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      idx.push(i);
+      empty = false;
+    }
+  }
+
+  return empty ? [-1, -1] : [Math.min(...idx), Math.max(...idx)];
 };
