4665b940fb433c7122bd2f6da0a002aa6724bfc7
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-17 17:52:04 -0800

    implement medium quesiton 38

--- Medium/countAndSay.js
+++ Medium/countAndSay.js
@@ -2,6 +2,28 @@
  * @param {number} n
  * @return {string}
  */
-const countAndSay = function(n) {
-    
+const countAndSay = function (n) {
+  let str = "1";
+
+  while (n > 1) {
+    let newStr = "",
+      count = 0,
+      say = str[0];
+
+    for (let i = 0; i < str.length; i += 1) {
+      if (str[i] === say) {
+        count += 1;
+      } else {
+        newStr += count + say;
+        count = 1;
+        say = str[i];
+      }
+    }
+
+    str = newStr + count + say;
+    n -= 1;
+  }
+  return str;
 };
+
+console.log(countAndSay(4));
