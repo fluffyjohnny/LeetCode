9cb0d27ba4bba1fbb4fa721f39e335f424430191
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-21 11:09:01 -0800

    implement medium question 48

--- Medium/rotate.js
+++ Medium/rotate.js
@@ -2,6 +2,14 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-const rotate = function(matrix) {
-  
+const rotate = function (matrix) {
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = r; c < matrix[0].length; c++) {
+      [matrix[r][c], matrix[c][r]] = [matrix[c][r], matrix[r][c]];
+    }
+  }
+
+  for (let row of matrix) {
+    row.reverse();
+  }
 };
