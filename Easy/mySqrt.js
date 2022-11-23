46ef785a79a6bc620bd6c74085124664a78c95a7
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-22 23:23:12 -0800

    implement easy question 69 with binary search

--- Easy/mySqrt.js
+++ Easy/mySqrt.js
@@ -2,6 +2,17 @@
  * @param {number} x
  * @return {number}
  */
-const mySqrt = function(x) {
-    
+const mySqrt = function (x) {
+  let left = 0;
+  let right = x;
+  let mid;
+
+  while (left < right) {
+    mid = Math.ceil((left + right) / 2);
+    if (mid * mid <= x && (mid + 1) * (mid + 1) > x) return mid; // 2.
+    if (mid * mid < x) left = mid; // 3.
+    else right = mid; // 4.
+  }
+
+  return right;
 };
