ac0fd536c55516b246f1c582db370316c634b1cf
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-25 10:43:56 -0800

    implement easy question 118

--- Easy/generate.js
+++ Easy/generate.js
@@ -2,6 +2,19 @@
  * @param {number} numRows
  * @return {number[][]}
  */
-const generate = function(numRows) {
-    
+const generate = function (numRows) {
+  const res = [];
+
+  for (let i = 0; i < numRows; i++) {
+    res[i] = [];
+    res[i][0] = 1;
+
+    for (let j = 1; j < i; j++) {
+      res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+      console.log(res[i - 1][j - 1], res[i - 1][j]);
+    }
+    res[i][i] = 1;
+  }
+
+  return res;
 };
