4ddd891d0d2655750fe31e783625eff5c1a4d875
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-28 09:55:11 -0800

    finish implementing easy question 136

--- Easy/singleNumber.js
+++ Easy/singleNumber.js
@@ -2,6 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-const singleNumber = function(nums) {
-    
+const singleNumber = function (nums) {
+  const temp = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!temp[nums[i]]) {
+      temp[nums[i]] = true;
+    } else {
+      delete temp[nums[i]];
+    }
+  }
+
+  return Object.keys(temp)[0];
 };
+
+console.log(singleNumber([2, 2, 1]));
