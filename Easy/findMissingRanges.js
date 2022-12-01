1da4448582514e01e07f35a048482c4521b8a4bb
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-11-30 16:27:33 -0800

    implement easyquestion 163

--- Easy/findMissingRanges.js
+++ Easy/findMissingRanges.js
@@ -4,6 +4,19 @@
  * @param {number} upper
  * @return {string[]}
  */
-const findMissingRanges = function(nums, lower, upper) {
-    
+const findMissingRanges = function (nums, lower, upper) {
+  const res = [];
+  nums = [lower - 1, ...nums, upper + 1];
+
+  for (let i = 1; i < nums.length; i++) {
+    const diff = nums[i] - nums[i - 1];
+
+    if (diff === 2) {
+      res.push(`${nums[i - 1] + 1}`);
+    } else if (diff > 2) {
+      res.push(`${nums[i - 1] + 1}->${nums[i] - 1}`);
+    }
+  }
+
+  return res;
 };
