93c79f47987f5e6e8975697435493e07855e66ff
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-12-06 10:48:45 -0800

    implement easy question 203

--- Easy/isHappy.js
+++ Easy/isHappy.js
@@ -2,6 +2,22 @@
  * @param {number} n
  * @return {boolean}
  */
-const isHappy = function(n) {
-    
+const isHappy = function (n) {
+  const map = {};
+  let num;
+
+  while (!map[n]) {
+    map[n] = true;
+
+    num = 0;
+    while (n > 0) {
+      num += (n % 10) * (n % 10);
+      n = Math.floor(n / 10);
+    }
+    if (num === 1) {
+      return true;
+    }
+    n = num;
+  }
+  return false;
 };
